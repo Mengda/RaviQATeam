1e52ae0c64f4240d25330244379e425f753ebe1b
Mengda Yang <mengda.yang@gmail.com>
2013-11-07 13:52:25 -0500

    Add main class \'Program\'

--- src/edu/cmu/lti/RaviQA/Program.java
+++ src/edu/cmu/lti/RaviQA/Program.java
@@ -1,15 +1,22 @@
 package edu.cmu.lti.RaviQA;
+
 import java.util.*;
+
 public class Program {
-	HashMap<String, ArrayList<String>> kwdArticalMap = Utils.LoadIndex();
-	
-	// Repeat
-	String input  = System.console().readLine();
-	ArrayList<ArrayList<String>> entities = Utils.GetEntities(input);
-	ArrayList<Article> candidateArticalList = Utils.GetArtical(kwdArticalMap, entities);
-	ArrayList<Sentence> candidateSentenceList = Utils.GetCandSentence(candidateArticalList, entities);
-	ArrayList<Candidate> candidateAnswerList = Utils.GetCandAns(candidateSentenceList, entities);
-	// Print output
-	
-	// End of Repeat
+	public static void main(String[] args) {
+		HashMap<String, ArrayList<String>> kwdArticalMap = Utils.LoadIndex();
+
+		// Repeat
+		String input = System.console().readLine();
+		ArrayList<ArrayList<String>> entities = Utils.GetEntities(input);
+		ArrayList<Article> candidateArticalList = Utils.GetArtical(
+				kwdArticalMap, entities);
+		ArrayList<Sentence> candidateSentenceList = Utils.GetCandSentence(
+				candidateArticalList, entities);
+		ArrayList<Candidate> candidateAnswerList = Utils.GetCandAns(
+				candidateSentenceList, entities);
+		// Print output
+
+		// End of Repeat
+	}
 }
