7c80182004cdee6e83540c1e269d6196eeb85979
Mengda Yang <mengda.yang@gmail.com>
2013-11-07 13:33:26 -0500

    Applied maven, first version of GetCandSentence

---
+++ src/edu/cmu/lti/RaviQA/ArticleProcessor.java
@@ -0,0 +1,75 @@
+package edu.cmu.lti.RaviQA;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import org.apache.commons.lang.StringUtils;
+
+public class ArticleProcessor {
+
+	public static ArrayList<Sentence> GetCandSentence(
+			ArrayList<Article> candidateArticalList,
+			ArrayList<ArrayList<String>> entities) throws Exception {
+
+		ArrayList<Sentence> answer = new ArrayList<Sentence>();
+
+		for (Article article : candidateArticalList) {
+			String fileName = "articles." + article.fileName;
+
+			File fXmlFile = new File(fileName);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+
+			printNote(doc.getChildNodes(), entities, answer);
+
+		}
+
+		return answer;
+	}
+
+	private static void printNote(NodeList nodeList,
+			ArrayList<ArrayList<String>> entities, ArrayList<Sentence> answer) {
+
+		for (int count = 0; count < nodeList.getLength(); count++) {
+
+			Node tempNode = nodeList.item(count);
+
+			// make sure it's element node.
+			if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				String[] sentences = tempNode.getTextContent().split(".");
+				for (String sentence : sentences) {
+					Integer match = 0;
+					for (String s : entities.get(0)) {
+						match = match + StringUtils.countMatches(sentence, s);
+					}
+					for(String s : Utils.GetSynonym(entities.get(1).get(0))){
+						match = match + StringUtils.countMatches(sentence, s) * 10;
+					}
+					if(match!=0){
+						answer.add(new Sentence(sentence,new Double(match)));
+					}
+				}
+				
+				if (tempNode.hasChildNodes()) {
+
+					// loop again if has child nodes
+					printNote(tempNode.getChildNodes(), entities, answer);
+
+				}
+
+			}
+
+		}
+	}
+}
--- src/edu/cmu/lti/RaviQA/Utils.java
+++ src/edu/cmu/lti/RaviQA/Utils.java
@@ -3,13 +3,13 @@ package edu.cmu.lti.RaviQA;
 import java.util.*;
 
 public class Utils {
-	static public ArrayList<ArrayList<String>> GetEntities(String input){
-		
+	static public ArrayList<ArrayList<String>> GetEntities(String input) {
+
 		return null;
 	}
-	
-	static public HashMap<String, ArrayList<String>> LoadIndex(){
-		
+
+	static public HashMap<String, ArrayList<String>> LoadIndex() {
+
 		return null;
 	}
 
@@ -23,7 +23,12 @@ public class Utils {
 	public static ArrayList<Sentence> GetCandSentence(
 			ArrayList<Article> candidateArticalList,
 			ArrayList<ArrayList<String>> entities) {
-		// TODO Auto-generated method stub
+		try {
+			return ArticleProcessor.GetCandSentence(candidateArticalList, entities);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
@@ -32,5 +37,15 @@ public class Utils {
 			ArrayList<ArrayList<String>> entities) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public static ArrayList<String> GetSynonym(String word){
+		ArrayList<String> answer = new ArrayList<String>();
+		
+		answer.add(word);
+		// TODO add synonym dictionary.
+		
+		return answer;
+		
 	}
 }
